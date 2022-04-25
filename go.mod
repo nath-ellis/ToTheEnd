6bcd3af45c1c3cb447796ea69a991fe6755cbc54
nathan <101135961+nath-ellis@users.noreply.github.com>
2022-04-25 20:21:03 +0100

    Made the Randomize function have more outcomes and fixed the import

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/gofrs/flock v0.8.1 // indirect
 	github.com/jezek/xgb v1.0.0 // indirect
 	github.com/solarlune/resolv v0.5.1
-	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a // indirect
 	golang.org/x/mobile v0.0.0-20220325161704-447654d348e3 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
--- tiles/randomize.go
+++ tiles/randomize.go
@@ -1,17 +1,51 @@
 package tiles
 
 import (
+	"math/rand"
+
 	"github.com/nath-ellis/ToTheEnd/data"
-	"golang.org/x/exp/rand"
 )
 
 func Randomize() {
+	chance := rand.Intn(5)
+	collectiveYPos := rand.Intn(data.SCREEN_HEIGHT - 64)
+
 	for _, t := range Tiles {
 		data.Space.Remove(t.Obj) // removes it
 
 		// Randomizes the position of the tiles
-		t.Obj.X = float64(rand.Intn(960))
-		t.Obj.Y = float64(rand.Intn(536))
+		// Numbers are 64 less than screen width and screen height to make sure no tiles are off the screen
+		if chance <= 1 {
+			// Completely random
+			t.Obj.X = float64(rand.Intn(data.SCREEN_WIDTH - 64))
+			t.Obj.Y = float64(rand.Intn(data.SCREEN_HEIGHT - 64))
+		} else if chance == 2 {
+			// All in a line but some in random places
+			t.Obj.X = float64(rand.Intn(data.SCREEN_WIDTH - 64))
+
+			chance2 := rand.Intn(10)
+			if (chance2 % 2) == 0 {
+				t.Obj.Y = float64(collectiveYPos)
+			} else {
+				t.Obj.Y = float64(data.SCREEN_HEIGHT - 64)
+			}
+		} else if chance == 3 {
+			// Spread apart from a line
+			t.Obj.X = float64(rand.Intn(data.SCREEN_WIDTH - 64))
+
+			amount := rand.Intn(256)
+			chance2 := rand.Intn(2)
+
+			if chance2 <= 1 {
+				t.Obj.Y = float64(collectiveYPos - amount)
+			} else {
+				t.Obj.Y = float64(collectiveYPos + amount)
+			}
+		} else if chance == 4 {
+			// All completely in a line
+			t.Obj.X = float64(rand.Intn(data.SCREEN_WIDTH - 64))
+			t.Obj.Y = float64(collectiveYPos)
+		}
 
 		data.Space.Add(t.Obj) // adds it back in the new position
 	}
