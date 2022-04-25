ea7f1a2fe5e6bd973102d9855bd02b56ee28aad8
nathan <101135961+nath-ellis@users.noreply.github.com>
2022-04-25 19:34:29 +0100

    Improved the randomize function

--- tiles/randomize.go
+++ tiles/randomize.go
@@ -1,10 +1,18 @@
 package tiles
 
-import "golang.org/x/exp/rand"
+import (
+	"github.com/nath-ellis/ToTheEnd/space"
+	"golang.org/x/exp/rand"
+)
 
 func Randomize() {
 	for _, t := range Tiles {
-		t.Obj.X = float64(rand.Intn(960))
-		t.Obj.Y = float64(rand.Intn(536))
+		space.Space.Remove(t.Obj) // removes it
+
+		t.Obj.X = float64(rand.Intn(960)) // randomizes x pos
+		t.Obj.Y = float64(rand.Intn(536)) // randomizes y pos
+		// Numbers are 64 less than screen width and screen height to make sure no tiles are off the screen
+
+		space.Space.Add(t.Obj) // adds it back in the new position
 	}
 }
